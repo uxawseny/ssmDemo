769ff60ed849cea7c6b9624259de95d537b3645b
uxawseny <uxawseny@gmail.com>
2016-10-11 22:33:22 +0800

    sql文件

--- pct.sql
+++ pct.sql
@@ -2,15 +2,10 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50709
 Source Host           : localhost:3306
 Source Database       : pct
-
 Target Server Type    : MYSQL
-Target Server Version : 50709
 File Encoding         : 65001
-
-Date: 2016-09-20 11:30:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
